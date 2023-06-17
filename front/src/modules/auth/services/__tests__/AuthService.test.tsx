c625e4c9840eeee98d457a58f18b698154c5efa1
Jérémy M <jeremy.magrin@gmail.com>
2023-06-17 13:42:02 +0200

    feat: refactoring auth & add email password login (#318)
    * feat: wip
    
    * fix: issues
    
    * feat: clean controllers and services
    
    * fix: test
    
    * Fix auth
    
    ---------
    
    Co-authored-by: Charles Bochet <charles@twenty.com>

--- front/src/modules/auth/services/__tests__/AuthService.test.tsx
+++ front/src/modules/auth/services/__tests__/AuthService.test.tsx
@@ -1,29 +1,62 @@
 import { waitFor } from '@testing-library/react';
 
 import {
+  getTokensFromLoginToken,
+  getTokensFromRefreshToken,
   getUserIdFromToken,
   hasAccessToken,
   hasRefreshToken,
-  refreshAccessToken,
 } from '../AuthService';
+
+const validTokensPayload = {
+  accessToken: {
+    token:
+      '***REMOVED***',
+    expiresAt: '2023-06-17T09:18:02.942Z',
+  },
+  refreshToken: {
+    token:
+      '***REMOVED***',
+    expiresAt: '2023-09-15T09:13:02.952Z',
+  },
+};
 
 const mockFetch = async (
   input: RequestInfo | URL,
   init?: RequestInit,
 ): Promise<Response> => {
-  const refreshToken = init?.body
-    ? JSON.parse(init.body.toString()).refreshToken
-    : null;
-  return new Promise((resolve) => {
-    resolve(
-      new Response(
-        JSON.stringify({
-          accessToken:
-            refreshToken === 'xxx-valid-refresh' ? 'xxx-valid-access' : null,
-        }),
-      ),
-    );
-  });
+  if (input.toString().match(/\/auth\/token$/g)) {
+    const refreshToken = init?.body
+      ? JSON.parse(init.body.toString()).refreshToken
+      : null;
+    return new Promise((resolve) => {
+      resolve(
+        new Response(
+          JSON.stringify({
+            tokens:
+              refreshToken === 'xxx-valid-refresh' ? validTokensPayload : null,
+          }),
+        ),
+      );
+    });
+  }
+
+  if (input.toString().match(/\/auth\/verify$/g)) {
+    const loginToken = init?.body
+      ? JSON.parse(init.body.toString()).loginToken
+      : null;
+    return new Promise((resolve) => {
+      resolve(
+        new Response(
+          JSON.stringify({
+            tokens:
+              loginToken === 'xxx-valid-login' ? validTokensPayload : null,
+          }),
+        ),
+      );
+    });
+  }
+  return new Promise(() => new Response());
 };
 
 global.fetch = mockFetch;
@@ -47,21 +80,28 @@ it('hasRefreshToken is true when token is not', () => {
 });
 
 it('refreshToken does not refresh the token if refresh token is missing', () => {
-  refreshAccessToken();
+  getTokensFromRefreshToken();
   expect(localStorage.getItem('accessToken')).toBeNull();
 });
 
 it('refreshToken does not refreh the token if refresh token is invalid', () => {
   localStorage.setItem('refreshToken', 'xxx-invalid-refresh');
-  refreshAccessToken();
+  getTokensFromRefreshToken();
+  expect(localStorage.getItem('accessToken')).toBeNull();
+});
+
+it('refreshToken does not refreh the token if refresh token is empty', () => {
+  getTokensFromRefreshToken();
   expect(localStorage.getItem('accessToken')).toBeNull();
 });
 
 it('refreshToken refreshes the token if refresh token is valid', async () => {
   localStorage.setItem('refreshToken', 'xxx-valid-refresh');
-  refreshAccessToken();
+  getTokensFromRefreshToken();
   await waitFor(() => {
-    expect(localStorage.getItem('accessToken')).toBe('xxx-valid-access');
+    expect(localStorage.getItem('accessToken')).toBe(
+      '***REMOVED***',
+    );
   });
 });
 
@@ -82,7 +122,29 @@ it('getUserIdFromToken returns the right userId when the token is valid', async 
     '***REMOVED***',
   );
   const userId = getUserIdFromToken();
-  expect(userId).toBe('b7590db9-ac7d-4275-9c6c-323979192795');
+  expect(userId).toBe('374fe3a5-df1e-4119-afe0-2a62a2ba481e');
+});
+
+it('getTokensFromLoginToken does nothing if loginToken is empty', async () => {
+  await getTokensFromLoginToken('');
+  expect(localStorage.getItem('accessToken')).toBeNull();
+  expect(localStorage.getItem('refreshToken')).toBeNull();
+});
+
+it('getTokensFromLoginToken does nothing if loginToken is not valid', async () => {
+  await getTokensFromLoginToken('xxx-invalid-login');
+  expect(localStorage.getItem('accessToken')).toBeNull();
+  expect(localStorage.getItem('refreshToken')).toBeNull();
+});
+
+it('getTokensFromLoginToken does nothing if loginToken is not valid', async () => {
+  await getTokensFromLoginToken('xxx-valid-login');
+  expect(localStorage.getItem('accessToken')).toBe(
+    '***REMOVED***',
+  );
+  expect(localStorage.getItem('refreshToken')).toBe(
+    '***REMOVED***',
+  );
 });
 
 afterEach(() => {
