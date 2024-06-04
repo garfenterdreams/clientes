twentyhq/twenty

commit 6898c1e4d8133e5321509d7d5cedcc28f190ed2f

fix: resolve issues with edit input offset (#5426)
## Description

This PR fixes a display issue when editing the company name or the name
of a person where the edit input would be offset to the left instead of
being in the middle.


![image](https://github.com/twentyhq/twenty/assets/36916632/beb91dc1-2d3c-46a5-93aa-f8189913fece)

Fixes #5416

### Type of change

<!-- Please delete options that are not relevant. -->

- [x] Bug fix (non-breaking change which fixes an issue)

## Checklist

- [x] My code follows the style guidelines of this project
- [x] I have performed a self-review of my own code
- [x] I have commented my code, particularly in hard-to-understand areas
- [x] I have made corresponding changes to the documentation
- [x] My changes generate no new warnings
- [x] I have added tests that prove my fix is effective or that my
feature works
- [x] New and existing unit tests pass locally with my changes
- [x] Any dependent changes have been merged and published in downstream
modules

packages/twenty-front/src/modules/object-record/record-inline-cell/components/RecordInlineCellEditMode.tsx
@@ -30,11 +30,10 @@ export const RecordInlineCellEditMode = ({
   children,
 }: RecordInlineCellEditModeProps) => {
   const { refs, floatingStyles } = useFloating({
-    placement: 'right',
     middleware: [
       flip(),
       offset({
-        crossAxis: -0.5,
+        mainAxis: -30,
       }),
     ],
     whileElementsMounted: autoUpdate,

garfenterdreams/clientes

commit 4aad3b4f56cc86ae9e0edf234dded567c7def414

Fix field input offset (#5726)
Fix issue introduced in https://github.com/twentyhq/twenty/pull/5426

It's not a beautiful solution. Maybe one day we should have a dedicated
component for title but it also comes with downsides (lot of code to
copy paste, such as "esc" to leave field, copy button, etc.). This one
doesn't create less debt in my opinion. Once we have the layout/widget
system we might have a dedicated widget type and the right abstraction
layers

packages/twenty-front/src/modules/object-record/record-field/contexts/FieldContext.ts
@@ -29,6 +29,7 @@ export type GenericFieldContextType = {
   basePathToShowPage?: string;
   clearable?: boolean;
   maxWidth?: number;
+  isCentered?: boolean;
 };
 
 export const FieldContext = createContext<GenericFieldContextType>(

packages/twenty-front/src/modules/object-record/record-inline-cell/components/RecordInlineCellEditMode.tsx
@@ -1,8 +1,11 @@
+import { useContext } from 'react';
 import { createPortal } from 'react-dom';
 import styled from '@emotion/styled';
 import { autoUpdate, flip, offset, useFloating } from '@floating-ui/react';
 
-const StyledInlineCellEditModeContainer = styled.div<RecordInlineCellEditModeProps>`
+import { FieldContext } from '@/object-record/record-field/contexts/FieldContext';
+
+const StyledInlineCellEditModeContainer = styled.div`
   align-items: center;
 
   display: flex;
@@ -29,12 +32,22 @@ type RecordInlineCellEditModeProps = {
 export const RecordInlineCellEditMode = ({
   children,
 }: RecordInlineCellEditModeProps) => {
+  const { isCentered } = useContext(FieldContext);
+
   const { refs, floatingStyles } = useFloating({
+    placement: isCentered ? undefined : 'right-start',
     middleware: [
       flip(),
-      offset({
-        mainAxis: -30,
-      }),
+      offset(
+        isCentered
+          ? {
+              mainAxis: -30,
+            }
+          : {
+              crossAxis: -4,
+              mainAxis: -4,
+            },
+      ),
     ],
     whileElementsMounted: autoUpdate,
   });

packages/twenty-front/src/modules/object-record/record-inline-cell/components/RecordInlineCellValue.tsx
@@ -23,11 +23,11 @@ const StyledClickableContainer = styled.div<{ readonly?: boolean }>`
 
 type RecordInlineCellValueProps = Pick<
   RecordInlineCellContainerProps,
-  | 'editModeContent'
   | 'displayModeContent'
   | 'customEditHotkeyScope'
-  | 'isDisplayModeContentEmpty'
+  | 'editModeContent'
   | 'editModeContentOnly'
+  | 'isDisplayModeContentEmpty'
   | 'isDisplayModeFixHeight'
   | 'disableHoverEffect'
   | 'readonly'

packages/twenty-front/src/modules/object-record/record-show/components/RecordShowContainer.tsx
@@ -168,6 +168,7 @@ export const RecordShowContainer = ({
                   },
                   useUpdateRecord: useUpdateOneObjectRecordMutation,
                   hotkeyScope: InlineCellHotkeyScope.InlineCell,
+                  isCentered: true,
                 }}
               >
                 <RecordInlineCell readonly={isReadOnly} />