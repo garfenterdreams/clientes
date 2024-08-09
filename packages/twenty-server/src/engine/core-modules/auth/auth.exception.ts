twentyhq/twenty

commit 2abb6adb6145695679339e93fbac846f407d2ad9

Build exceptions and handler (#6459)
Adding exceptions and handler for auth services.

Tested with:
- Workspace creation
- Workspace signup
- Workspace invitation
- Reset password
- Adding email account
- Impersonation

---------

Co-authored-by: Weiko <corentin@twenty.com>

packages/twenty-server/src/engine/core-modules/auth/auth.exception.ts
@@ -1,0 +1,17 @@
+import { CustomException } from 'src/utils/custom-exception';
+
+export class AuthException extends CustomException {
+  code: AuthExceptionCode;
+  constructor(message: string, code: AuthExceptionCode) {
+    super(message, code);
+  }
+}
+
+export enum AuthExceptionCode {
+  USER_NOT_FOUND = 'USER_NOT_FOUND',
+  CLIENT_NOT_FOUND = 'CLIENT_NOT_FOUND',
+  INVALID_INPUT = 'INVALID_INPUT',
+  FORBIDDEN_EXCEPTION = 'FORBIDDEN_EXCEPTION',
+  INVALID_DATA = 'INVALID_DATA',
+  INTERNAL_SERVER_ERROR = 'INTERNAL_SERVER_ERROR',
+}

packages/twenty-server/src/engine/core-modules/auth/filters/auth-graphql-api-exception.filter.ts
@@ -1,0 +1,31 @@
+import { Catch } from '@nestjs/common';
+
+import {
+  AuthException,
+  AuthExceptionCode,
+} from 'src/engine/core-modules/auth/auth.exception';
+import {
+  ForbiddenError,
+  InternalServerError,
+  NotFoundError,
+  UserInputError,
+} from 'src/engine/core-modules/graphql/utils/graphql-errors.util';
+
+@Catch(AuthException)
+export class AuthGraphqlApiExceptionFilter {
+  catch(exception: AuthException) {
+    switch (exception.code) {
+      case AuthExceptionCode.USER_NOT_FOUND:
+      case AuthExceptionCode.CLIENT_NOT_FOUND:
+        throw new NotFoundError(exception.message);
+      case AuthExceptionCode.INVALID_INPUT:
+        throw new UserInputError(exception.message);
+      case AuthExceptionCode.FORBIDDEN_EXCEPTION:
+        throw new ForbiddenError(exception.message);
+      case AuthExceptionCode.INVALID_DATA:
+      case AuthExceptionCode.INTERNAL_SERVER_ERROR:
+      default:
+        throw new InternalServerError(exception.message);
+    }
+  }
+}

packages/twenty-server/src/engine/core-modules/auth/services/token.service.ts
@@ -1,12 +1,4 @@
-import {
-  BadRequestException,
-  ForbiddenException,
-  Injectable,
-  InternalServerErrorException,
-  NotFoundException,
-  UnauthorizedException,
-  UnprocessableEntityException,
-} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import crypto from 'crypto';
@@ -24,6 +16,10 @@ import {
   AppToken,
   AppTokenType,
 } from 'src/engine/core-modules/app-token/app-token.entity';
+import {
+  AuthException,
+  AuthExceptionCode,
+} from 'src/engine/core-modules/auth/auth.exception';
 import { EmailPasswordResetLink } from 'src/engine/core-modules/auth/dto/email-password-reset-link.entity';
 import { ExchangeAuthCode } from 'src/engine/core-modules/auth/dto/exchange-auth-code.entity';
 import { ExchangeAuthCodeInput } from 'src/engine/core-modules/auth/dto/exchange-auth-code.input';
@@ -45,7 +41,6 @@ import { User } from 'src/engine/core-modules/user/user.entity';
 import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
 import { EmailService } from 'src/engine/integrations/email/email.service';
 import { EnvironmentService } from 'src/engine/integrations/environment/environment.service';
-import { assert } from 'src/utils/assert';
 
 @Injectable()
 export class TokenService {
@@ -68,7 +63,13 @@ export class TokenService {
   ): Promise<AuthToken> {
     const expiresIn = this.environmentService.get('ACCESS_TOKEN_EXPIRES_IN');
 
-    assert(expiresIn, '', InternalServerErrorException);
+    if (!expiresIn) {
+      throw new AuthException(
+        'Expiration time for access token is not set',
+        AuthExceptionCode.INTERNAL_SERVER_ERROR,
+      );
+    }
+
     const expiresAt = addMilliseconds(new Date().getTime(), ms(expiresIn));
 
     const user = await this.userRepository.findOne({
@@ -77,11 +78,17 @@ export class TokenService {
     });
 
     if (!user) {
-      throw new NotFoundException('User is not found');
+      throw new AuthException(
+        'User is not found',
+        AuthExceptionCode.INVALID_INPUT,
+      );
     }
 
     if (!user.defaultWorkspace) {
-      throw new NotFoundException('User does not have a default workspace');
+      throw new AuthException(
+        'User does not have a default workspace',
+        AuthExceptionCode.INVALID_DATA,
+      );
     }
 
     const jwtPayload: JwtPayload = {
@@ -99,7 +106,13 @@ export class TokenService {
     const secret = this.environmentService.get('REFRESH_TOKEN_SECRET');
     const expiresIn = this.environmentService.get('REFRESH_TOKEN_EXPIRES_IN');
 
-    assert(expiresIn, '', InternalServerErrorException);
+    if (!expiresIn) {
+      throw new AuthException(
+        'Expiration time for access token is not set',
+        AuthExceptionCode.INTERNAL_SERVER_ERROR,
+      );
+    }
+
     const expiresAt = addMilliseconds(new Date().getTime(), ms(expiresIn));
 
     const refreshTokenPayload = {
@@ -130,7 +143,13 @@ export class TokenService {
     const secret = this.environmentService.get('LOGIN_TOKEN_SECRET');
     const expiresIn = this.environmentService.get('LOGIN_TOKEN_EXPIRES_IN');
 
-    assert(expiresIn, '', InternalServerErrorException);
+    if (!expiresIn) {
+      throw new AuthException(
+        'Expiration time for access token is not set',
+        AuthExceptionCode.INTERNAL_SERVER_ERROR,
+      );
+    }
+
     const expiresAt = addMilliseconds(new Date().getTime(), ms(expiresIn));
     const jwtPayload = {
       sub: email,
@@ -155,7 +174,13 @@ export class TokenService {
       'SHORT_TERM_TOKEN_EXPIRES_IN',
     );
 
-    assert(expiresIn, '', InternalServerErrorException);
+    if (!expiresIn) {
+      throw new AuthException(
+        'Expiration time for access token is not set',
+        AuthExceptionCode.INTERNAL_SERVER_ERROR,
+      );
+    }
+
     const expiresAt = addMilliseconds(new Date().getTime(), ms(expiresIn));
     const jwtPayload = {
       sub: workspaceMemberId,
@@ -212,7 +237,10 @@ export class TokenService {
     const token = ExtractJwt.fromAuthHeaderAsBearerToken()(request);
 
     if (!token) {
-      throw new UnauthorizedException('missing authentication token');
+      throw new AuthException(
+        'missing authentication token',
+        AuthExceptionCode.FORBIDDEN_EXCEPTION,
+      );
     }
     const decoded = await this.verifyJwt(
       token,
@@ -257,22 +285,35 @@ export class TokenService {
   ): Promise<AuthTokens> {
     const userExists = await this.userRepository.findBy({ id: user.id });
 
-    assert(userExists, 'User not found', NotFoundException);
+    if (!userExists) {
+      throw new AuthException(
+        'User not found',
+        AuthExceptionCode.INVALID_INPUT,
+      );
+    }
 
     const workspace = await this.workspaceRepository.findOne({
       where: { id: workspaceId },
       relations: ['workspaceUsers'],
     });
 
-    assert(workspace, 'workspace doesnt exist', NotFoundException);
+    if (!workspace) {
+      throw new AuthException(
+        'workspace doesnt exist',
+        AuthExceptionCode.INVALID_INPUT,
+      );
+    }
 
-    assert(
-      workspace.workspaceUsers
+    if (
+      !workspace.workspaceUsers
         .map((userWorkspace) => userWorkspace.userId)
-        .includes(user.id),
-      'user does not belong to workspace',
-      ForbiddenException,
-    );
+        .includes(user.id)
+    ) {
+      throw new AuthException(
+        'user does not belong to workspace',
+        AuthExceptionCode.FORBIDDEN_EXCEPTION,
+      );
+    }
 
     await this.userRepository.save({
       id: user.id,
@@ -293,28 +334,16 @@ export class TokenService {
   async verifyAuthorizationCode(
     exchangeAuthCodeInput: ExchangeAuthCodeInput,
   ): Promise<ExchangeAuthCode> {
-    const { authorizationCode, codeVerifier, clientSecret } =
-      exchangeAuthCodeInput;
+    const { authorizationCode, codeVerifier } = exchangeAuthCodeInput;
 
-    assert(
-      authorizationCode,
-      'Authorization code not found',
-      NotFoundException,
-    );
-
-    assert(
-      !codeVerifier || !clientSecret,
-      'client secret or code verifier not found',
-      NotFoundException,
-    );
+    if (!authorizationCode) {
+      throw new AuthException(
+        'Authorization code not found',
+        AuthExceptionCode.INVALID_INPUT,
+      );
+    }
 
     let userId = '';
-
-    if (clientSecret) {
-      // TODO: replace this with call to third party apps table
-      // assert(client.secret, 'client secret code does not exist', ForbiddenException);
-      throw new ForbiddenException();
-    }
 
     if (codeVerifier) {
       const authorizationCodeAppToken = await this.appTokenRepository.findOne({
@@ -323,17 +352,19 @@ export class TokenService {
         },
       });
 
-      assert(
-        authorizationCodeAppToken,
-        'Authorization code does not exist',
-        NotFoundException,
-      );
+      if (!authorizationCodeAppToken) {
+        throw new AuthException(
+          'Authorization code does not exist',
+          AuthExceptionCode.INVALID_INPUT,
+        );
+      }
 
-      assert(
-        authorizationCodeAppToken.expiresAt.getTime() >= Date.now(),
-        'Authorization code expired.',
-        ForbiddenException,
-      );
+      if (!(authorizationCodeAppToken.expiresAt.getTime() >= Date.now())) {
+        throw new AuthException(
+          'Authorization code expired.',
+          AuthExceptionCode.FORBIDDEN_EXCEPTION,
+        );
+      }
 
       const codeChallenge = crypto
         .createHash('sha256')
@@ -350,26 +381,32 @@ export class TokenService {
         },
       });
 
-      assert(
-        codeChallengeAppToken,
-        'code verifier doesnt match the challenge',
-        ForbiddenException,
-      );
+      if (!codeChallengeAppToken) {
+        throw new AuthException(
+          'code verifier doesnt match the challenge',
+          AuthExceptionCode.FORBIDDEN_EXCEPTION,
+        );
+      }
 
-      assert(
-        codeChallengeAppToken.expiresAt.getTime() >= Date.now(),
-        'code challenge expired.',
-        ForbiddenException,
-      );
+      if (!(codeChallengeAppToken.expiresAt.getTime() >= Date.now())) {
+        throw new AuthException(
+          'code challenge expired.',
+          AuthExceptionCode.FORBIDDEN_EXCEPTION,
+        );
+      }
 
-      assert(
-        codeChallengeAppToken.userId === authorizationCodeAppToken.userId,
-        'authorization code / code verifier was not created by same client',
-        ForbiddenException,
-      );
+      if (codeChallengeAppToken.userId !== authorizationCodeAppToken.userId) {
+        throw new AuthException(
+          'authorization code / code verifier was not created by same client',
+          AuthExceptionCode.FORBIDDEN_EXCEPTION,
+        );
+      }
 
       if (codeChallengeAppToken.revokedAt) {
-        throw new ForbiddenException('Token has been revoked.');
+        throw new AuthException(
+          'Token has been revoked.',
+          AuthExceptionCode.FORBIDDEN_EXCEPTION,
+        );
       }
 
       await this.appTokenRepository.save({
@@ -386,13 +423,17 @@ export class TokenService {
     });
 
     if (!user) {
-      throw new NotFoundException(
+      throw new AuthException(
         'User who generated the token does not exist',
+        AuthExceptionCode.INVALID_INPUT,
       );
     }
 
     if (!user.defaultWorkspace) {
-      throw new NotFoundException('User does not have a default workspace');
+      throw new AuthException(
+        'User does not have a default workspace',
+        AuthExceptionCode.INVALID_DATA,
+      );
     }
 
     const accessToken = await this.generateAccessToken(
@@ -414,24 +455,35 @@ export class TokenService {
     const coolDown = this.environmentService.get('REFRESH_TOKEN_COOL_DOWN');
     const jwtPayload = await this.verifyJwt(refreshToken, secret);
 
-    assert(
-      jwtPayload.jti && jwtPayload.sub,
-      'This refresh token is malformed',
-      UnprocessableEntityException,
-    );
+    if (!(jwtPayload.jti && jwtPayload.sub)) {
+      throw new AuthException(
+        'This refresh token is malformed',
+        AuthExceptionCode.INVALID_INPUT,
+      );
+    }
 
     const token = await this.appTokenRepository.findOneBy({
       id: jwtPayload.jti,
     });
 
-    assert(token, "This refresh token doesn't exist", NotFoundException);
+    if (!token) {
+      throw new AuthException(
+        "This refresh token doesn't exist",
+        AuthExceptionCode.INVALID_INPUT,
+      );
+    }
 
     const user = await this.userRepository.findOne({
       where: { id: jwtPayload.sub },
       relations: ['appTokens'],
     });
 
-    assert(user, 'User not found', NotFoundException);
+    if (!user) {
+      throw new AuthException(
+        'User not found',
+        AuthExceptionCode.INVALID_INPUT,
+      );
+    }
 
     // Check if revokedAt is less than coolDown
     if (
@@ -452,8 +504,9 @@ export class TokenService {
         }),
       );
 
-      throw new ForbiddenException(
-        'Suspicious activity detected, this refresh token has been revoked. All tokens has been revoked.',
+      throw new AuthException(
+        'Suspicious activity detected, this refresh token has been revoked. All tokens have been revoked.',
+        AuthExceptionCode.FORBIDDEN_EXCEPTION,
       );
     }
 
@@ -464,6 +517,13 @@ export class TokenService {
     accessToken: AuthToken;
     refreshToken: AuthToken;
   }> {
+    if (!token) {
+      throw new AuthException(
+        'Refresh token not found',
+        AuthExceptionCode.INVALID_INPUT,
+      );
+    }
+
     const {
       user,
       token: { id },
@@ -502,11 +562,20 @@ export class TokenService {
       );
     } catch (error) {
       if (error instanceof TokenExpiredError) {
-        throw new UnauthorizedException('Token has expired.');
+        throw new AuthException(
+          'Token has expired.',
+          AuthExceptionCode.FORBIDDEN_EXCEPTION,
+        );
       } else if (error instanceof JsonWebTokenError) {
-        throw new UnauthorizedException('Token invalid.');
+        throw new AuthException(
+          'Token invalid.',
+          AuthExceptionCode.FORBIDDEN_EXCEPTION,
+        );
       } else {
-        throw new UnprocessableEntityException();
+        throw new AuthException(
+          'Unknown token error.',
+          AuthExceptionCode.INVALID_INPUT,
+        );
       }
     }
   }
@@ -516,17 +585,23 @@ export class TokenService {
       email,
     });
 
-    assert(user, 'User not found', NotFoundException);
+    if (!user) {
+      throw new AuthException(
+        'User not found',
+        AuthExceptionCode.INVALID_INPUT,
+      );
+    }
 
     const expiresIn = this.environmentService.get(
       'PASSWORD_RESET_TOKEN_EXPIRES_IN',
     );
 
-    assert(
-      expiresIn,
-      'PASSWORD_RESET_TOKEN_EXPIRES_IN constant value not found',
-      InternalServerErrorException,
-    );
+    if (!expiresIn) {
+      throw new AuthException(
+        'PASSWORD_RESET_TOKEN_EXPIRES_IN constant value not found',
+        AuthExceptionCode.INTERNAL_SERVER_ERROR,
+      );
+    }
 
     const existingToken = await this.appTokenRepository.findOne({
       where: {
@@ -543,10 +618,9 @@ export class TokenService {
         { long: true },
       );
 
-      assert(
-        false,
+      throw new AuthException(
         `Token has already been generated. Please wait for ${timeToWait} to generate again.`,
-        BadRequestException,
+        AuthExceptionCode.INVALID_INPUT,
       );
     }
 
@@ -579,7 +653,12 @@ export class TokenService {
       email,
     });
 
-    assert(user, 'User not found', NotFoundException);
+    if (!user) {
+      throw new AuthException(
+        'User not found',
+        AuthExceptionCode.INVALID_INPUT,
+      );
+    }
 
     const frontBaseURL = this.environmentService.get('FRONT_BASE_URL');
     const resetLink = `${frontBaseURL}/reset-password/${resetToken.passwordResetToken}`;
@@ -636,13 +715,23 @@ export class TokenService {
       },
     });
 
-    assert(token, 'Token is invalid', NotFoundException);
+    if (!token) {
+      throw new AuthException(
+        'Token is invalid',
+        AuthExceptionCode.FORBIDDEN_EXCEPTION,
+      );
+    }
 
     const user = await this.userRepository.findOneBy({
       id: token.userId,
     });
 
-    assert(user, 'Token is invalid', NotFoundException);
+    if (!user) {
+      throw new AuthException(
+        'User not found',
+        AuthExceptionCode.INVALID_INPUT,
+      );
+    }
 
     return {
       id: user.id,
@@ -657,7 +746,12 @@ export class TokenService {
       id: userId,
     });
 
-    assert(user, 'User not found', NotFoundException);
+    if (!user) {
+      throw new AuthException(
+        'User not found',
+        AuthExceptionCode.INVALID_INPUT,
+      );
+    }
 
     await this.appTokenRepository.update(
       {

packages/twenty-server/src/engine/core-modules/auth/auth.resolver.ts
@@ -1,14 +1,5 @@
-import {
-  BadRequestException,
-  ForbiddenException,
-  InternalServerErrorException,
-  NotFoundException,
-  UseGuards,
-} from '@nestjs/common';
+import { UseFilters, UseGuards } from '@nestjs/common';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { InjectRepository } from '@nestjs/typeorm';
-
-import { Repository } from 'typeorm';
 
 import { ApiKeyTokenInput } from 'src/engine/core-modules/auth/dto/api-key-token.input';
 import { AppTokenInput } from 'src/engine/core-modules/auth/dto/app-token.input';
@@ -24,6 +15,7 @@ import { TransientToken } from 'src/engine/core-modules/auth/dto/transient-token
 import { UpdatePasswordViaResetTokenInput } from 'src/engine/core-modules/auth/dto/update-password-via-reset-token.input';
 import { ValidatePasswordResetToken } from 'src/engine/core-modules/auth/dto/validate-password-reset-token.entity';
 import { ValidatePasswordResetTokenInput } from 'src/engine/core-modules/auth/dto/validate-password-reset-token.input';
+import { AuthGraphqlApiExceptionFilter } from 'src/engine/core-modules/auth/filters/auth-graphql-api-exception.filter';
 import { UserService } from 'src/engine/core-modules/user/services/user.service';
 import { User } from 'src/engine/core-modules/user/user.entity';
 import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
@@ -31,7 +23,6 @@ import { AuthUser } from 'src/engine/decorators/auth/auth-user.decorator';
 import { AuthWorkspace } from 'src/engine/decorators/auth/auth-workspace.decorator';
 import { JwtAuthGuard } from 'src/engine/guards/jwt.auth.guard';
 import { CaptchaGuard } from 'src/engine/integrations/captcha/captcha.guard';
-import { assert } from 'src/utils/assert';
 
 import { ChallengeInput } from './dto/challenge.input';
 import { ImpersonateInput } from './dto/impersonate.input';
@@ -48,10 +39,9 @@ import { AuthService } from './services/auth.service';
 import { TokenService } from './services/token.service';
 
 @Resolver()
+@UseFilters(AuthGraphqlApiExceptionFilter)
 export class AuthResolver {
   constructor(
-    @InjectRepository(Workspace, 'core')
-    private readonly workspaceRepository: Repository<Workspace>,
     private authService: AuthService,
     private tokenService: TokenService,
     private userService: UserService,
@@ -81,16 +71,10 @@ export class AuthResolver {
   @Query(() => Workspace)
   async findWorkspaceFromInviteHash(
     @Args() workspaceInviteHashValidInput: WorkspaceInviteHashValidInput,
-  ) {
-    const workspace = await this.workspaceRepository.findOneBy({
-      inviteHash: workspaceInviteHashValidInput.inviteHash,
-    });
-
-    if (!workspace) {
-      throw new BadRequestException('Workspace does not exist');
-    }
-
-    return workspace;
+  ): Promise<Workspace> {
+    return await this.authService.findWorkspaceFromInviteHashOrFail(
+      workspaceInviteHashValidInput.inviteHash,
+    );
   }
 
   @UseGuards(CaptchaGuard)
@@ -151,8 +135,6 @@ export class AuthResolver {
       verifyInput.loginToken,
     );
 
-    assert(email, 'Invalid token', ForbiddenException);
-
     const result = await this.authService.verify(email);
 
     return result;
@@ -188,10 +170,6 @@ export class AuthResolver {
 
   @Mutation(() => AuthTokens)
   async renewToken(@Args() args: AppTokenInput): Promise<AuthTokens> {
-    if (!args.appToken) {
-      throw new BadRequestException('Refresh token is mendatory');
-    }
-
     const tokens = await this.tokenService.generateTokensFromRefreshToken(
       args.appToken,
     );
@@ -205,10 +183,7 @@ export class AuthResolver {
     @Args() impersonateInput: ImpersonateInput,
     @AuthUser() user: User,
   ): Promise<Verify> {
-    // Check if user can impersonate
-    assert(user.canImpersonate, 'User cannot impersonate', ForbiddenException);
-
-    return this.authService.impersonate(impersonateInput.userId);
+    return await this.authService.impersonate(impersonateInput.userId, user);
   }
 
   @UseGuards(JwtAuthGuard)
@@ -240,20 +215,13 @@ export class AuthResolver {
 
   @Mutation(() => InvalidatePassword)
   async updatePasswordViaResetToken(
-    @Args() args: UpdatePasswordViaResetTokenInput,
+    @Args()
+    { passwordResetToken, newPassword }: UpdatePasswordViaResetTokenInput,
   ): Promise<InvalidatePassword> {
-    const { id } = await this.tokenService.validatePasswordResetToken(
-      args.passwordResetToken,
-    );
+    const { id } =
+      await this.tokenService.validatePasswordResetToken(passwordResetToken);
 
-    assert(id, 'User not found', NotFoundException);
-
-    const { success } = await this.authService.updatePassword(
-      id,
-      args.newPassword,
-    );
-
-    assert(success, 'Password update failed', InternalServerErrorException);
+    await this.authService.updatePassword(id, newPassword);
 
     return await this.tokenService.invalidatePasswordResetToken(id);
   }

packages/twenty-server/src/engine/core-modules/auth/controllers/google-apis-auth.controller.ts
@@ -3,12 +3,17 @@ import {
   Get,
   Req,
   Res,
-  UnauthorizedException,
+  UseFilters,
   UseGuards,
 } from '@nestjs/common';
 
 import { Response } from 'express';
 
+import {
+  AuthException,
+  AuthExceptionCode,
+} from 'src/engine/core-modules/auth/auth.exception';
+import { AuthRestApiExceptionFilter } from 'src/engine/core-modules/auth/filters/auth-rest-api-exception.filter';
 import { GoogleAPIsOauthExchangeCodeForTokenGuard } from 'src/engine/core-modules/auth/guards/google-apis-oauth-exchange-code-for-token.guard';
 import { GoogleAPIsOauthRequestCodeGuard } from 'src/engine/core-modules/auth/guards/google-apis-oauth-request-code.guard';
 import { GoogleAPIsService } from 'src/engine/core-modules/auth/services/google-apis.service';
@@ -19,6 +24,7 @@ import { EnvironmentService } from 'src/engine/integrations/environment/environm
 import { LoadServiceWithWorkspaceContext } from 'src/engine/twenty-orm/context/load-service-with-workspace.context';
 
 @Controller('auth/google-apis')
+@UseFilters(AuthRestApiExceptionFilter)
 export class GoogleAPIsAuthController {
   constructor(
     private readonly googleAPIsService: GoogleAPIsService,
@@ -59,13 +65,17 @@ export class GoogleAPIsAuthController {
     const demoWorkspaceIds = this.environmentService.get('DEMO_WORKSPACE_IDS');
 
     if (demoWorkspaceIds.includes(workspaceId)) {
-      throw new UnauthorizedException(
+      throw new AuthException(
         'Cannot connect Google account to demo workspace',
+        AuthExceptionCode.FORBIDDEN_EXCEPTION,
       );
     }
 
     if (!workspaceId) {
-      throw new Error('Workspace not found');
+      throw new AuthException(
+        'Workspace not found',
+        AuthExceptionCode.INVALID_INPUT,
+      );
     }
 
     const handle = emails[0].value;

garfenterdreams/clientes

commit 5600ebd73de3f262a88252193b598ed1a37e03ed

Fix auth exceptions (#6590)
A regression has been introduced in
https://github.com/twentyhq/twenty/pull/6459/files#diff-0a06bf2b624f77f1b7ded0fcc4ce266d1a56f4329222b46d1cf4d76a18000c97L505
where 401 have been changed to 403. However the renew token logic on the
FE expects a 401, see here
https://github.com/twentyhq/twenty/blob/main/packages/twenty-front/src/modules/apollo/services/apollo.factory.ts#L100

I've also introduced a fix with a proxy class in
GraphQLHydrateRequestFromTokenMiddleware since this middleware calls
validateToken from tokenService which are never converted back to
graphqlErrors so handleExceptionAndConvertToGraphQLError below will
receive an AuthException and will send capture it and return it as a 500

both issues have been fixed and should resolve the renewToken logic 

## Test
tested locally by playing with token expiration dates in the env

<img width="635" alt="Screenshot 2024-08-09 at 12 47 05"
src="https://github.com/user-attachments/assets/e93d5741-29b7-4699-b882-ee8f4fee6e6e">

packages/twenty-server/src/engine/core-modules/auth/auth.exception.ts
@@ -12,6 +12,7 @@ export enum AuthExceptionCode {
   CLIENT_NOT_FOUND = 'CLIENT_NOT_FOUND',
   INVALID_INPUT = 'INVALID_INPUT',
   FORBIDDEN_EXCEPTION = 'FORBIDDEN_EXCEPTION',
+  UNAUTHENTICATED = 'UNAUTHENTICATED',
   INVALID_DATA = 'INVALID_DATA',
   INTERNAL_SERVER_ERROR = 'INTERNAL_SERVER_ERROR',
 }

packages/twenty-server/src/engine/core-modules/auth/filters/auth-graphql-api-exception.filter.ts
@@ -1,10 +1,11 @@
-import { Catch } from '@nestjs/common';
+import { Catch, ExceptionFilter } from '@nestjs/common';
 
 import {
   AuthException,
   AuthExceptionCode,
 } from 'src/engine/core-modules/auth/auth.exception';
 import {
+  AuthenticationError,
   ForbiddenError,
   InternalServerError,
   NotFoundError,
@@ -12,7 +13,7 @@ import {
 } from 'src/engine/core-modules/graphql/utils/graphql-errors.util';
 
 @Catch(AuthException)
-export class AuthGraphqlApiExceptionFilter {
+export class AuthGraphqlApiExceptionFilter implements ExceptionFilter {
   catch(exception: AuthException) {
     switch (exception.code) {
       case AuthExceptionCode.USER_NOT_FOUND:
@@ -22,6 +23,8 @@ export class AuthGraphqlApiExceptionFilter {
         throw new UserInputError(exception.message);
       case AuthExceptionCode.FORBIDDEN_EXCEPTION:
         throw new ForbiddenError(exception.message);
+      case AuthExceptionCode.UNAUTHENTICATED:
+        throw new AuthenticationError(exception.message);
       case AuthExceptionCode.INVALID_DATA:
       case AuthExceptionCode.INTERNAL_SERVER_ERROR:
       default:

packages/twenty-server/src/engine/core-modules/auth/services/token.service.ts
@@ -564,12 +564,12 @@ export class TokenService {
       if (error instanceof TokenExpiredError) {
         throw new AuthException(
           'Token has expired.',
-          AuthExceptionCode.FORBIDDEN_EXCEPTION,
+          AuthExceptionCode.UNAUTHENTICATED,
         );
       } else if (error instanceof JsonWebTokenError) {
         throw new AuthException(
           'Token invalid.',
-          AuthExceptionCode.FORBIDDEN_EXCEPTION,
+          AuthExceptionCode.UNAUTHENTICATED,
         );
       } else {
         throw new AuthException(

packages/twenty-server/src/engine/middlewares/graphql-hydrate-request-from-token.middleware.ts
@@ -1,12 +1,31 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 
-import { Request, Response, NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
+import { AuthGraphqlApiExceptionFilter } from 'src/engine/core-modules/auth/filters/auth-graphql-api-exception.filter';
 import { TokenService } from 'src/engine/core-modules/auth/services/token.service';
 import { AuthContext } from 'src/engine/core-modules/auth/types/auth-context.type';
 import { ExceptionHandlerService } from 'src/engine/integrations/exception-handler/exception-handler.service';
 import { WorkspaceCacheVersionService } from 'src/engine/metadata-modules/workspace-cache-version/workspace-cache-version.service';
 import { handleExceptionAndConvertToGraphQLError } from 'src/engine/utils/global-exception-handler.util';
+
+class GraphqlTokenValidationProxy {
+  private tokenService: TokenService;
+
+  constructor(tokenService: TokenService) {
+    this.tokenService = tokenService;
+  }
+
+  async validateToken(req: Request) {
+    try {
+      return await this.tokenService.validateToken(req);
+    } catch (error) {
+      const authGraphqlApiExceptionFilter = new AuthGraphqlApiExceptionFilter();
+
+      throw authGraphqlApiExceptionFilter.catch(error);
+    }
+  }
+}
 
 @Injectable()
 export class GraphQLHydrateRequestFromTokenMiddleware
@@ -48,7 +67,11 @@ export class GraphQLHydrateRequestFromTokenMiddleware
     let data: AuthContext;
 
     try {
-      data = await this.tokenService.validateToken(req);
+      const graphqlTokenValidationProxy = new GraphqlTokenValidationProxy(
+        this.tokenService,
+      );
+
+      data = await graphqlTokenValidationProxy.validateToken(req);
       const cacheVersion = await this.workspaceCacheVersionService.getVersion(
         data.workspace.id,
       );