twentyhq/twenty

commit 6432ad39b9aee2c606fd9080ca409a43be600c7c

feat: add new ACTOR field type and createdBy standard fields (#6324)
This pull request introduces a new `FieldMetadataType` called `ACTOR`.
The primary objective of this new type is to add an extra column to the
following objects: `person`, `company`, `opportunity`, `note`, `task`,
and all custom objects.

This composite type contains three properties:

- `source`
    ```typescript
    export enum FieldActorSource {
      EMAIL = 'EMAIL',
      CALENDAR = 'CALENDAR',
      API = 'API',
      IMPORT = 'IMPORT',
      MANUAL = 'MANUAL',
    }
    ```
- `workspaceMemberId`
- This property can be `undefined` in some cases and refers to the
member who created the record.
- `name`
- Serves as a fallback if the `workspaceMember` is deleted and is used
for other source types like `API`.

### Functionality

The pre-hook system has been updated to allow real-time argument
updates. When a record is created, a pre-hook can now compute and update
the arguments accordingly. This enhancement enables the `createdBy`
field to be populated with the correct values based on the
`authContext`.

The `authContext` now includes:
- An optional User entity
- An optional ApiKey entity
- The workspace entity

This provides access to the necessary data for the `createdBy` field.

In the GraphQL API, only the `source` can be specified in the
`createdBy` input. This allows the front-end to specify the source when
creating records from a CSV file.

### Front-End Handling

On the front-end, `orderBy` and `filter` are only applied to the name
property of the `ACTOR` composite type. Currently, we are unable to
apply these operations to the workspace member relation. This means that
if a workspace member changes their first name or last name, there may
be a mismatch because the name will differ from the new one. The name
displayed on the screen is based on the workspace member entity when
available.

### Missing Components

packages/twenty-server/src/engine/api/graphql/workspace-resolver-builder/factories/create-many-resolver.factory.ts
@@ -25,12 +25,11 @@ export class CreateManyResolverFactory
   ): Resolver<CreateManyResolverArgs> {
     const internalContext = context;
 
-    return async (_source, args, context, info) => {
+    return (_source, args, context, info) => {
       try {
-        return await this.workspaceQueryRunnerService.createMany(args, {
+        return this.workspaceQueryRunnerService.createMany(args, {
+          authContext: internalContext.authContext,
           objectMetadataItem: internalContext.objectMetadataItem,
-          workspaceId: internalContext.workspaceId,
-          userId: internalContext.userId,
           info,
           fieldMetadataCollection: internalContext.fieldMetadataCollection,
           objectMetadataCollection: internalContext.objectMetadataCollection,

packages/twenty-server/src/engine/api/graphql/workspace-resolver-builder/factories/create-one-resolver.factory.ts
@@ -25,12 +25,11 @@ export class CreateOneResolverFactory
   ): Resolver<CreateOneResolverArgs> {
     const internalContext = context;
 
-    return async (_source, args, context, info) => {
+    return (_source, args, context, info) => {
       try {
-        return await this.workspaceQueryRunnerService.createOne(args, {
+        return this.workspaceQueryRunnerService.createOne(args, {
+          authContext: internalContext.authContext,
           objectMetadataItem: internalContext.objectMetadataItem,
-          workspaceId: internalContext.workspaceId,
-          userId: internalContext.userId,
           info,
           fieldMetadataCollection: internalContext.fieldMetadataCollection,
           objectMetadataCollection: internalContext.objectMetadataCollection,

packages/twenty-server/src/engine/api/graphql/workspace-resolver-builder/factories/delete-many-resolver.factory.ts
@@ -25,12 +25,11 @@ export class DeleteManyResolverFactory
   ): Resolver<DeleteManyResolverArgs> {
     const internalContext = context;
 
-    return async (_source, args, context, info) => {
+    return (_source, args, context, info) => {
       try {
-        return await this.workspaceQueryRunnerService.deleteMany(args, {
+        return this.workspaceQueryRunnerService.deleteMany(args, {
+          authContext: internalContext.authContext,
           objectMetadataItem: internalContext.objectMetadataItem,
-          workspaceId: internalContext.workspaceId,
-          userId: internalContext.userId,
           info,
           fieldMetadataCollection: internalContext.fieldMetadataCollection,
           objectMetadataCollection: internalContext.objectMetadataCollection,

packages/twenty-server/src/engine/api/graphql/workspace-resolver-builder/factories/delete-one-resolver.factory.ts
@@ -25,12 +25,11 @@ export class DeleteOneResolverFactory
   ): Resolver<DeleteOneResolverArgs> {
     const internalContext = context;
 
-    return async (_source, args, context, info) => {
+    return (_source, args, context, info) => {
       try {
-        return await this.workspaceQueryRunnerService.deleteOne(args, {
+        return this.workspaceQueryRunnerService.deleteOne(args, {
+          authContext: internalContext.authContext,
           objectMetadataItem: internalContext.objectMetadataItem,
-          workspaceId: internalContext.workspaceId,
-          userId: internalContext.userId,
           info,
           fieldMetadataCollection: internalContext.fieldMetadataCollection,
           objectMetadataCollection: internalContext.objectMetadataCollection,

packages/twenty-server/src/engine/api/graphql/workspace-resolver-builder/factories/find-duplicates-resolver.factory.ts
@@ -25,12 +25,11 @@ export class FindDuplicatesResolverFactory
   ): Resolver<FindDuplicatesResolverArgs> {
     const internalContext = context;
 
-    return async (_source, args, context, info) => {
+    return (_source, args, context, info) => {
       try {
-        return await this.workspaceQueryRunnerService.findDuplicates(args, {
+        return this.workspaceQueryRunnerService.findDuplicates(args, {
+          authContext: internalContext.authContext,
           objectMetadataItem: internalContext.objectMetadataItem,
-          workspaceId: internalContext.workspaceId,
-          userId: internalContext.userId,
           info,
           fieldMetadataCollection: internalContext.fieldMetadataCollection,
           objectMetadataCollection: internalContext.objectMetadataCollection,

packages/twenty-server/src/engine/api/graphql/workspace-resolver-builder/factories/find-many-resolver.factory.ts
@@ -25,12 +25,11 @@ export class FindManyResolverFactory
   ): Resolver<FindManyResolverArgs> {
     const internalContext = context;
 
-    return async (_source, args, context, info) => {
+    return (_source, args, context, info) => {
       try {
-        return await this.workspaceQueryRunnerService.findMany(args, {
+        return this.workspaceQueryRunnerService.findMany(args, {
+          authContext: internalContext.authContext,
           objectMetadataItem: internalContext.objectMetadataItem,
-          workspaceId: internalContext.workspaceId,
-          userId: internalContext.userId,
           info,
           fieldMetadataCollection: internalContext.fieldMetadataCollection,
           objectMetadataCollection: internalContext.objectMetadataCollection,

packages/twenty-server/src/engine/api/graphql/workspace-resolver-builder/factories/find-one-resolver.factory.ts
@@ -25,12 +25,11 @@ export class FindOneResolverFactory
   ): Resolver<FindOneResolverArgs> {
     const internalContext = context;
 
-    return async (_source, args, context, info) => {
+    return (_source, args, context, info) => {
       try {
-        return await this.workspaceQueryRunnerService.findOne(args, {
+        return this.workspaceQueryRunnerService.findOne(args, {
+          authContext: internalContext.authContext,
           objectMetadataItem: internalContext.objectMetadataItem,
-          workspaceId: internalContext.workspaceId,
-          userId: internalContext.userId,
           info,
           fieldMetadataCollection: internalContext.fieldMetadataCollection,
           objectMetadataCollection: internalContext.objectMetadataCollection,

packages/twenty-server/src/engine/api/graphql/workspace-resolver-builder/factories/update-many-resolver.factory.ts
@@ -25,12 +25,11 @@ export class UpdateManyResolverFactory
   ): Resolver<UpdateManyResolverArgs> {
     const internalContext = context;
 
-    return async (_source, args, context, info) => {
+    return (_source, args, context, info) => {
       try {
-        return await this.workspaceQueryRunnerService.updateMany(args, {
+        return this.workspaceQueryRunnerService.updateMany(args, {
+          authContext: internalContext.authContext,
           objectMetadataItem: internalContext.objectMetadataItem,
-          workspaceId: internalContext.workspaceId,
-          userId: internalContext.userId,
           info,
           fieldMetadataCollection: internalContext.fieldMetadataCollection,
           objectMetadataCollection: internalContext.objectMetadataCollection,

garfenterdreams/clientes

commit 06a76a223049a2c6080db86de6dc88b9ad2a69fa

Fix query runner exceptions not being handled properly (#6593)
## Context

We recently introduced a try catch in the different resolvers of the
query runner to handle exceptions via
workspaceQueryRunnerGraphqlApiExceptionHandler and convert them to
proper errors. However this was never called as expected because query
runner methods were async.
This is a regression from https://github.com/twentyhq/twenty/pull/6324

## Before

<img width="938" alt="Screenshot 2024-08-09 at 15 34 02"
src="https://github.com/user-attachments/assets/3607c7ed-ea91-4729-a4e4-ede7761347e2">


## After

<img width="905" alt="Screenshot 2024-08-09 at 15 33 46"
src="https://github.com/user-attachments/assets/51bcbfa1-9b0b-4c7c-84a2-7c8effeadddd">

packages/twenty-server/src/engine/api/graphql/workspace-resolver-builder/factories/create-many-resolver.factory.ts
@@ -25,9 +25,9 @@ export class CreateManyResolverFactory
   ): Resolver<CreateManyResolverArgs> {
     const internalContext = context;
 
-    return (_source, args, context, info) => {
+    return async (_source, args, context, info) => {
       try {
-        return this.workspaceQueryRunnerService.createMany(args, {
+        return await this.workspaceQueryRunnerService.createMany(args, {
           authContext: internalContext.authContext,
           objectMetadataItem: internalContext.objectMetadataItem,
           info,

packages/twenty-server/src/engine/api/graphql/workspace-resolver-builder/factories/create-one-resolver.factory.ts
@@ -25,9 +25,9 @@ export class CreateOneResolverFactory
   ): Resolver<CreateOneResolverArgs> {
     const internalContext = context;
 
-    return (_source, args, context, info) => {
+    return async (_source, args, context, info) => {
       try {
-        return this.workspaceQueryRunnerService.createOne(args, {
+        return await this.workspaceQueryRunnerService.createOne(args, {
           authContext: internalContext.authContext,
           objectMetadataItem: internalContext.objectMetadataItem,
           info,

packages/twenty-server/src/engine/api/graphql/workspace-resolver-builder/factories/delete-many-resolver.factory.ts
@@ -25,9 +25,9 @@ export class DeleteManyResolverFactory
   ): Resolver<DeleteManyResolverArgs> {
     const internalContext = context;
 
-    return (_source, args, context, info) => {
+    return async (_source, args, context, info) => {
       try {
-        return this.workspaceQueryRunnerService.deleteMany(args, {
+        return await this.workspaceQueryRunnerService.deleteMany(args, {
           authContext: internalContext.authContext,
           objectMetadataItem: internalContext.objectMetadataItem,
           info,

packages/twenty-server/src/engine/api/graphql/workspace-resolver-builder/factories/delete-one-resolver.factory.ts
@@ -25,9 +25,9 @@ export class DeleteOneResolverFactory
   ): Resolver<DeleteOneResolverArgs> {
     const internalContext = context;
 
-    return (_source, args, context, info) => {
+    return async (_source, args, context, info) => {
       try {
-        return this.workspaceQueryRunnerService.deleteOne(args, {
+        return await this.workspaceQueryRunnerService.deleteOne(args, {
           authContext: internalContext.authContext,
           objectMetadataItem: internalContext.objectMetadataItem,
           info,

packages/twenty-server/src/engine/api/graphql/workspace-resolver-builder/factories/find-duplicates-resolver.factory.ts
@@ -25,9 +25,9 @@ export class FindDuplicatesResolverFactory
   ): Resolver<FindDuplicatesResolverArgs> {
     const internalContext = context;
 
-    return (_source, args, context, info) => {
+    return async (_source, args, context, info) => {
       try {
-        return this.workspaceQueryRunnerService.findDuplicates(args, {
+        return await this.workspaceQueryRunnerService.findDuplicates(args, {
           authContext: internalContext.authContext,
           objectMetadataItem: internalContext.objectMetadataItem,
           info,

packages/twenty-server/src/engine/api/graphql/workspace-resolver-builder/factories/find-many-resolver.factory.ts
@@ -25,9 +25,9 @@ export class FindManyResolverFactory
   ): Resolver<FindManyResolverArgs> {
     const internalContext = context;
 
-    return (_source, args, context, info) => {
+    return async (_source, args, context, info) => {
       try {
-        return this.workspaceQueryRunnerService.findMany(args, {
+        return await this.workspaceQueryRunnerService.findMany(args, {
           authContext: internalContext.authContext,
           objectMetadataItem: internalContext.objectMetadataItem,
           info,

packages/twenty-server/src/engine/api/graphql/workspace-resolver-builder/factories/find-one-resolver.factory.ts
@@ -25,9 +25,9 @@ export class FindOneResolverFactory
   ): Resolver<FindOneResolverArgs> {
     const internalContext = context;
 
-    return (_source, args, context, info) => {
+    return async (_source, args, context, info) => {
       try {
-        return this.workspaceQueryRunnerService.findOne(args, {
+        return await this.workspaceQueryRunnerService.findOne(args, {
           authContext: internalContext.authContext,
           objectMetadataItem: internalContext.objectMetadataItem,
           info,

packages/twenty-server/src/engine/api/graphql/workspace-resolver-builder/factories/update-many-resolver.factory.ts
@@ -25,9 +25,9 @@ export class UpdateManyResolverFactory
   ): Resolver<UpdateManyResolverArgs> {
     const internalContext = context;
 
-    return (_source, args, context, info) => {
+    return async (_source, args, context, info) => {
       try {
-        return this.workspaceQueryRunnerService.updateMany(args, {
+        return await this.workspaceQueryRunnerService.updateMany(args, {
           authContext: internalContext.authContext,
           objectMetadataItem: internalContext.objectMetadataItem,
           info,

packages/twenty-server/src/engine/api/graphql/workspace-resolver-builder/factories/update-one-resolver.factory.ts
@@ -25,9 +25,9 @@ export class UpdateOneResolverFactory
   ): Resolver<UpdateOneResolverArgs> {
     const internalContext = context;
 
-    return (_source, args, context, info) => {
+    return async (_source, args, context, info) => {
       try {
-        return this.workspaceQueryRunnerService.updateOne(args, {
+        return await this.workspaceQueryRunnerService.updateOne(args, {
           authContext: internalContext.authContext,
           objectMetadataItem: internalContext.objectMetadataItem,
           info,