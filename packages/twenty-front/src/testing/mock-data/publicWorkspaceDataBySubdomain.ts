34e334cf26daced2f4b0db746e1b9d942e2a2e5f
Antoine Moreaux <moreaux.antoine@gmail.com>
2025-02-07 14:34:26 +0100

    feat(): enable custom domain usage (#9911)
    # Content
    - Introduce the `workspaceUrls` property. It contains two
    sub-properties: `customUrl, subdomainUrl`. These endpoints are used to
    access the workspace. Even if the `workspaceUrls` is invalid for
    multiple reasons, the `subdomainUrl` remains valid.
    - Introduce `ResolveField` workspaceEndpoints to avoid unnecessary URL
    computation on the frontend part.
    - Add a `forceSubdomainUrl` to avoid custom URL using a query parameter

--- packages/twenty-front/src/testing/mock-data/publicWorkspaceDataBySubdomain.ts
+++ packages/twenty-front/src/testing/mock-data/publicWorkspaceDataBySubdomain.ts
@@ -1,12 +1,15 @@
-import { GetPublicWorkspaceDataBySubdomainQuery } from '~/generated/graphql';
+import { GetPublicWorkspaceDataByDomainQuery } from '~/generated/graphql';
 
-export const mockedPublicWorkspaceDataBySubdomain: GetPublicWorkspaceDataBySubdomainQuery['getPublicWorkspaceDataBySubdomain'] =
+export const mockedPublicWorkspaceDataBySubdomain: GetPublicWorkspaceDataByDomainQuery['getPublicWorkspaceDataByDomain'] =
   {
     __typename: 'PublicWorkspaceDataOutput',
     id: '9870323e-22c3-4d14-9b7f-5bdc84f7d6ee',
     logo: 'workspace-logo/original/c88deb49-7636-4560-918d-08c3265ffb20.49?token=***REMOVED***',
     displayName: 'Twenty Eng',
-    subdomain: 'twenty-eng',
+    workspaceUrls: {
+      customUrl: 'https://twenty-eng.com',
+      subdomainUrl: 'https://custom.twenty.com',
+    },
     authProviders: {
       __typename: 'AuthProviders',
       sso: [],
