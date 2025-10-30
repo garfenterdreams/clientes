9de835773b4d1adcf63219a17adb6b92fe8d5787
martmull <martmull@hotmail.fr>
2025-10-30 12:18:25 +0100

    Stop using .env variables to authenticate with cli (#15461)
    - as title, solves our user facing authentication issues using the
    twenty-cli
    - update twenty-cli version (breaking change from previous PR)

---
+++ packages/twenty-cli/src/__tests__/e2e/constants/testConfig.ts
@@ -0,0 +1,8 @@
+import { TwentyConfig } from '../../../types/config.types';
+
+export const testConfig: TwentyConfig = {
+  apiUrl: 'http://localhost:3000',
+  apiKey:
+    '***REMOVED***',
+  defaultApp: 'e2e-default-app',
+};
